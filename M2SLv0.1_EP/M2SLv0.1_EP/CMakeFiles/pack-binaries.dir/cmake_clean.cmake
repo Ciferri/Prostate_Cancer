33cec955820d6095fa4baa3c7ccd0f129936f2e2
Ciferri <ciferri56@gmail.com>
2017-05-16 15:51:05 +0200

    suppression dossiers

--- M2SLv0.1_EP/M2SLv0.1_EP/CMakeFiles/pack-binaries.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pack-binaries"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pack-binaries.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- M2SLv0.1_EP/M2SLv0.1_EP/modelType.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * Project SimCard
- * @(#) modelType.cpp
- * Author: Antoine Defontaine
- * antoine.defontaine@univ-rennes1.fr
- * $DATE
- */
-
-#include "modelType.h"
