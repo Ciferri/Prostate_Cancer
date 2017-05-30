c4fb4481f8696d620566b60bc9253fd4843803c3
Carlos Sosa Marrero <carlos.sosa@etudiant.univ-rennes1.fr>
2017-05-30 16:05:33 +0200

    tissue selected from command line and images no longer shown

--- printOutImg.m
+++ printOutImg.m
@@ -1,6 +1,6 @@
 clear all
 close all
-
+warning('off')
 pkg load image
 
 load('out.dat');
@@ -19,15 +19,14 @@ nlayer = dim(3);
 fclose(fid);
 
 n3 = size(out, 1);
-figure(1)
+%figure(1)
 for i = 1:n3
   for j = 1:nlayer
     matrix(:,:,(i-1)*nlayer+j) = reshape(out(i, (j-1)*nrow*ncol+1:j*nrow*ncol), ncol, nrow);
     img = uint16(matrix(:,:,(i-1)*nlayer+j)');
-    imshow(img, cmap)
+    %imshow(img, cmap)
     img = imresize(img,4,'nearest');
     path = ['img/figure' num2str((i-1)*nlayer+j,'%04d') '.png'];
     imwrite(img,cmap,path)
-    pause(0.1)
   end
 end
