2184634f0145f78abec2053e5dee1dd285b088d0
Carlos Sosa Marrero <carlos.sosa@etudiant.univ-rennes1.fr>
2017-05-31 16:19:00 +0200

    some formating done

--- ProstCell.hpp
+++ ProstCell.hpp
@@ -14,38 +14,38 @@
 #include "Treatment.hpp"
 
 //Inputs
-#define IN_TUM        m_in->at(0)
-#define IN_VES        m_in->at(1)
-#define IN_DEAD       m_in->at(2)
-#define IN_ALIVE      m_in->at(3)
-#define IN_PO2        m_in->at(4)
-#define IN_TIMER      m_in->at(5)
+#define IN_ALIVE     m_in->at(0)
+#define IN_TUM       m_in->at(1)
+#define IN_DEAD      m_in->at(2)
+#define IN_VES       m_in->at(3)
+#define IN_PO2       m_in->at(4)
+#define IN_TIMER     m_in->at(5)
 
 //State variables
-#define ST_ALIVE      m_st->at(0)
-#define ST_DEAD       m_st->at(1)
-#define ST_TUM        m_st->at(2)
-#define ST_VES        m_st->at(3)
-#define ST_G1         m_st->at(4)
-#define ST_S          m_st->at(5)
-#define ST_G2         m_st->at(6)
-#define ST_M          m_st->at(7)
+#define ST_ALIVE     m_st->at(0)
+#define ST_TUM       m_st->at(1)
+#define ST_DEAD      m_st->at(2)
+#define ST_VES       m_st->at(3)
+#define ST_G1        m_st->at(4)
+#define ST_S         m_st->at(5)
+#define ST_G2        m_st->at(6)
+#define ST_M         m_st->at(7)
 
 //Outputs
-#define OUT_STATE     m_out->at(0)
+#define OUT_STATE    m_out->at(0)
 
 //Internal Parameters
-#define PAR_DOUBTIME  m_param->at(0)
-#define PAR_LIMG1S    m_param->at(1)
-#define PAR_LIMSG2    m_param->at(2)
-#define PAR_LIMG2M    m_param->at(3)
-#define PAR_DEADTIME  m_param->at(4)
-#define PAR_TIMER     m_param->at(5)
-#define PAR_M         m_param->at(6)
-#define PAR_K         m_param->at(7)
-#define PAR_PO2       m_param->at(8)
-#define PAR_ALPHA     m_param->at(9)
-#define PAR_BETA      m_param->at(10)
+#define PAR_TIMER    m_param->at(0)
+#define PAR_DOUBTIME m_param->at(1)
+#define PAR_LIMG1S   m_param->at(2)
+#define PAR_LIMSG2   m_param->at(3)
+#define PAR_LIMG2M   m_param->at(4)
+#define PAR_DEADTIME m_param->at(5)
+#define PAR_M        m_param->at(6)
+#define PAR_K        m_param->at(7)
+#define PAR_PO2      m_param->at(8)
+#define PAR_ALPHA    m_param->at(9)
+#define PAR_BETA     m_param->at(10)
 
 class ProstCell: public Model{
 public :
