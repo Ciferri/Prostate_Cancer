a146d94dd4d78343609be324ae0ada1b92ff4382
Carlos Sosa Marrero <carlos.sosa@etudiant.univ-rennes1.fr>
2017-06-12 16:38:10 +0200

    coupler version

--- main.cpp
+++ main.cpp
@@ -14,36 +14,41 @@
 #include <stdlib.h>
 #include <string>
 
+#include "coupler.hpp"
+#include "diffusion3D.hpp"
 #include "Gen3DProstTissue.hpp"
 #include "Model.hpp"
+#include "prostateCell.hpp"
 #include "ProstCell.hpp"
+#include "rootSimulator.hpp"
 #include "Simulator.hpp"
 #include "Treatment.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-  Model *model;
+  Model *coupler, *model1, *model2;
   int nrow, ncol, nlayer;
-  double apopDeadTime, apopProb, doubTime, necDeadTime;
+  double doubTime;
+  double apopDeadTime, apopProb, necDeadTime;
+  double KConso, Vmax;
   string nFInPO2, nFInTum, nFInVes, nFTissueDim;
   vector<double> alpha(7, 0.0);
   vector<double> beta(7, 0.0);
   vector<double> cycDistrib(4, 0.0);
   vector<double> cycDur(4, 0.0);
   Treatment *treatment;
+  RootSimulator *sim;
+  double DT1, DT2, simTime;
 
-  Simulator *sim;
-  double simTime;
-
-  if(argc != 27){
-    cout << "Incorrect number of parameters. 26 parameters expected"
+  if(argc != 29){
+    cout << "Incorrect number of parameters. 28 parameters expected"
 	 << endl;
     cout << "doubTime G1Dur SDur G2Dur MDur G1Distrib SDistrib" <<
       "G2Distrib MDistrib alphaAlive alphaTumG1 alphaTumS" <<
       "alphaTumG2 alphaTumM alphaDead alphaVes betaAlive" <<
       "betaTumG1 betaTumS betaTumG2 betaTumM betaDead betaVes" <<
-      "apopProb apopDeadTime necDeadTime" << endl;
+      "apopProb apopDeadTime necDeadTime Vmax KConso" << endl;
     return EXIT_FAILURE;
     }
   
@@ -100,19 +105,28 @@ int main(int argc, char *argv[]){
   apopProb     = atof(argv[24]);
   apopDeadTime = atof(argv[25]);
   necDeadTime  = atof(argv[26]);
+
+  Vmax   = atof(argv[27]);
+  KConso = atof(argv[28]);
   
   treatment = new Treatment();
   cout<<treatment<<endl;
   //treatment = 0;
-  model = new Gen3DProstTissue(nrow, ncol, nlayer, nFInPO2, nFInTum,
-			       nFInVes, doubTime, cycDur,
-			       cycDistrib, apopDeadTime,
-			       necDeadTime, apopProb, alpha, beta,
-			       treatment);
-  sim = new Simulator();
+  model1 = new Gen3DProstTissue(nrow, ncol, nlayer, nFInPO2,
+				nFInTum, nFInVes, doubTime, cycDur,
+				cycDistrib, apopDeadTime,
+				necDeadTime, apopProb, alpha, beta,
+				treatment);
+  model2 = new diffusion3D(nrow, ncol, nlayer, nFInVes, Vmax,
+			   KConso);
+  coupler = new Coupler(model1, model2);
+  DT1 = 1; //h;
+  DT2 = 1; //s;
+  sim = new RootSimulator(coupler, DT1, DT2);
   simTime = treatment->getDuration();
   //simTime = 2016;
-  sim->setModel(model);
-  sim->start(simTime);
+  sim->initSim();
+  cout << "test2" << endl;
+  sim->simulate(0.0, simTime);
   return EXIT_SUCCESS;
 }
